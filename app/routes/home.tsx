202a9d52743169facc44d03b197b011ef1d18457
rohansingh2809 <rohansingh28092001@gmail.com>
2025-08-11 13:36:05 +0530

    auth done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,32 @@
+import { usePuterStore } from "~/lib/putter";
 import { resumes } from "../../constants";
-import type { Route } from "./+types/home";
+import type { Route } from "../routes.ts";
 import Navbar from "~/components/navbr";
 import ResumeCard from "~/components/ResumeCard";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "Smart resume checker" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
+const {auth}=usePuterStore();
+const navigate=useNavigate();
+
+ useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
   <Navbar/>
+ 
+
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Rating</h1>
         <h2>Review your submission and check AI-powered feedback</h2>
       </div>
